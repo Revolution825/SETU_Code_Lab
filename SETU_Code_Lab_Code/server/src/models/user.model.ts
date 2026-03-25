90a757641f8ce7b6aae556ce700e73f33bf255bf
Diarmuid ONeill <diarmuidoneill2003@gmail.com>
2026-03-25 21:19:47 +0000

    Fixed course results showing lecturer results also

--- SETU_Code_Lab_Code/server/src/models/user.model.ts
+++ SETU_Code_Lab_Code/server/src/models/user.model.ts
@@ -1,53 +1,63 @@
-import { pool } from "../infrastructure/database"
+import { pool } from "../infrastructure/database";
 
 export const fetchAllStudents = async () => {
-    const result = await pool.query(
-        "SELECT user_id AS student_id, user_name AS student_name FROM users WHERE role='student'", []
-    );
-    return result.rows;
-}
+  const result = await pool.query(
+    "SELECT user_id AS student_id, user_name AS student_name FROM users WHERE role='student'",
+    [],
+  );
+  return result.rows;
+};
 
 export const fetchStudentsOnCourse = async (course_id: number) => {
-    const result = await pool.query(
-        "SELECT u.user_id, u.user_name FROM users u JOIN enrollment e ON u.user_id = e.user_id WHERE e.course_id = $1", [course_id]
-    );
-    return result.rows;
-}
+  const result = await pool.query(
+    "SELECT u.user_id, u.user_name FROM users u JOIN enrollment e ON u.user_id = e.user_id WHERE e.course_id = $1 AND u.role = 'student'",
+    [course_id],
+  );
+  return result.rows;
+};
 
 export const fetchUserData = async (user_id: number) => {
-    const result = await pool.query(
-        "SELECT user_id, user_name, email, role, total_points, current_streak, longest_streak, last_solved_date FROM users WHERE user_id = $1", [user_id]
-    );
-    return result.rows[0];
-}
+  const result = await pool.query(
+    "SELECT user_id, user_name, email, role, total_points, current_streak, longest_streak, last_solved_date FROM users WHERE user_id = $1",
+    [user_id],
+  );
+  return result.rows[0];
+};
 
 export const deleteAccount = async (user_id: number) => {
-    await pool.query(
-        "DELETE FROM users WHERE user_id = $1", [user_id]
-    );
-}
+  await pool.query("DELETE FROM users WHERE user_id = $1", [user_id]);
+};
 
-export const updateUserPoints = async (client: any, user_id: number, points_awarded: number) => {
-    const result = await client.query(
-        `UPDATE users SET total_points = total_points + $1
+export const updateUserPoints = async (
+  client: any,
+  user_id: number,
+  points_awarded: number,
+) => {
+  const result = await client.query(
+    `UPDATE users SET total_points = total_points + $1
             WHERE user_id = $2`,
-        [points_awarded, user_id]
-    );
-    return result.rows[0]
-}
+    [points_awarded, user_id],
+  );
+  return result.rows[0];
+};
 
 export const getStreakData = async (client: any, user_id: number) => {
-    const result = await client.query(
-        `SELECT last_solved_date, current_streak, longest_streak
+  const result = await client.query(
+    `SELECT last_solved_date, current_streak, longest_streak
             FROM users
             WHERE user_id = $1`,
-        [user_id]
-    );
-    return result.rows[0];
-}
+    [user_id],
+  );
+  return result.rows[0];
+};
 
-export const updateStreak = async (client: any, user_id: number, currentStreak: number) => {
-    await client.query(
-        `UPDATE users SET current_streak=$1, longest_streak=GREATEST(longest_streak, $1), last_solved_date=CURRENT_DATE WHERE user_id=$2`, [currentStreak, user_id]
-    );
-}
+export const updateStreak = async (
+  client: any,
+  user_id: number,
+  currentStreak: number,
+) => {
+  await client.query(
+    `UPDATE users SET current_streak=$1, longest_streak=GREATEST(longest_streak, $1), last_solved_date=CURRENT_DATE WHERE user_id=$2`,
+    [currentStreak, user_id],
+  );
+};
