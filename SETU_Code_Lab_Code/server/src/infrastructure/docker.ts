aa20fbe1d9e90411f9f6dc02c47d7d376ca9b255
Diarmuid O'Neill <diarmuidoneill2003@gmail.com>
2026-04-10 13:19:34 +0100

    Changes for Azure hosting for Cloud module

--- SETU_Code_Lab_Code/server/src/infrastructure/docker.ts
+++ SETU_Code_Lab_Code/server/src/infrastructure/docker.ts
@@ -1,5 +1,9 @@
 import Docker from "dockerode";
 
-const docker = new Docker({socketPath: "/var/run/docker.sock"});
+const docker = new Docker(
+  process.env.DOCKER_HOST
+    ? { host: "localhost", port: 2375, protocol: "http" }
+    : { socketPath: "/var/run/docker.sock" },
+);
 
 export default docker;
